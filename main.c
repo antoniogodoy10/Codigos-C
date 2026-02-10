3e4a13f44dbd446555cb1ded77e021b9f614e959
antoniogodoy10 <godoyantonio735@gmail.com>
2026-02-10 19:50:45 -0300

    Exercicio de string

--- main.c
+++ main.c
@@ -1,43 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int N; // número de casos de teste
-    scanf("%d", &N);
 
-    for (int caso = 0; caso < N; caso++) {
-        int M; // número de alunos
-        scanf("%d", &M);
+    char texto[200];
+    int i, cont = 0;
 
-        int notas[M];
-        int copia[M];
+    printf ("Digite uma string: ");
+    fgets(texto, 200, stdin);
 
-        // leitura das notas e cópia
-        for (int i = 0; i < M; i++) {
-            scanf("%d", &notas[i]);
-            copia[i] = notas[i];
-        }
-
-        // ordenação decrescente (Bubble Sort)
-        for (int i = 0; i < M - 1; i++) {
-            for (int j = 0; j < M - 1 - i; j++) {
-                if (notas[j] < notas[j + 1]) {
-                    int aux = notas[j];
-                    notas[j] = notas[j + 1];
-                    notas[j + 1] = aux;
-                }
-            }
-        }
-
-        // contar quantos ficaram na mesma posição
-        int iguais = 0;
-        for (int i = 0; i < M; i++) {
-            if (notas[i] == copia[i]) {
-                iguais++;
-            }
-        }
-
-        printf("%d\n", iguais);
-    }
-
-    return 0;
+    
 }
